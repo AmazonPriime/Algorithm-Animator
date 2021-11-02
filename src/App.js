951fa3cec93426bd64379ff42f80e1a6a3e87fcf
Luke <2342915h@student.gla.ac.uk>
2021-11-02 16:15:59 +0000

    Updated the icon with a eye icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import logo from './resources/logo.svg';
 import './App.css';
 
 function App() {
@@ -9,22 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Header />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Main content
         <Footer />
       </header>
     </div>
