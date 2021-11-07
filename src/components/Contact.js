e387dba358e880fc087200a001e02c5a6080dd78
Luke <2342915h@student.gla.ac.uk>
2021-11-07 23:48:39 +0000

    Added form components
    Need to add functionality and play around with the style

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import './About.css';
+import './Contact.css';
 
 const about = () => (
   <div>
@@ -12,13 +12,17 @@ const about = () => (
       </span>
     </Modal.Header>
     <Modal.Body>
+      <span className="info">
+        Use the form below to send me an message, this can be anything about the website
+        a suggestion, bug report or anything else you can think of.
+      </span>
       <Form>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="name@example.com" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
+          <Form.Label>Message</Form.Label>
           <Form.Control as="textarea" rows={3} />
         </Form.Group>
         <Button variant="primary" type="submit">
