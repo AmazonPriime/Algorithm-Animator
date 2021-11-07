98a77df9158fae52f0acd55bcc94e3b2a557d516
Luke <2342915h@student.gla.ac.uk>
2021-11-07 23:04:28 +0000

    Updates to the config

--- src/constant/config.js
+++ src/constant/config.js
@@ -1,4 +1,6 @@
 module.exports = {
   developerUrl: 'https://github.com/AmazonPriime',
+  developerWebsite: 'https://amazonpriime.github.io/',
   sourceCode: 'https://github.com/AmazonPriime/Algorithm-Animator',
+  linkedin: 'https://www.linkedin.com/in/luke-holland-/',
 };
