fb05f50b6558fc4d523ec29b27b01c2ec0635670
Luke <2342915h@student.gla.ac.uk>
2021-11-16 23:36:34 +0000

    Started to play around with graph library Cytoscape

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import CytoscapeComponent from 'react-cytoscapejs';
 import './Graph.css';
 
-const graph = () => (
-  <div>
-    Graph
-  </div>
-);
+const graph = () => {
+  const elements = [
+    { data: { id: 'one', label: 'Node 1' }, position: { x: 200, y: 200 } },
+    { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 100 } },
+    { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } },
+  ];
+
+  return (
+    <div className="graph-container">
+      <CytoscapeComponent
+        elements={elements}
+        className="graph"
+      />
+    </div>
+  );
+};
 
 export default graph;
