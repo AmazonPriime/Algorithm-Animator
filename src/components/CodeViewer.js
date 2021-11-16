34e8e4ab49959fcc2269e8b4cd1949e5b2b83da8
Luke <2342915h@student.gla.ac.uk>
2021-11-16 21:39:07 +0000

    MAde some changes to start implemented the code viewer

--- src/components/CodeViewer.js
+++ src/components/CodeViewer.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './CodeViewer.css';
 
-const codeViewer = () => (
-  <div>
-    Code Viewer
-  </div>
-);
+const codeViewer = (props) => {
+  const { code } = props;
+  console.log(code);
+
+  return <div />;
+};
+
+codeViewer.defaultProps = {
+  code: '',
+};
 
 export default codeViewer;
+
+// <div id="codeContainer" className="code-container">
+//   <div id="code" className="code">
+//     <pre>{ code }</pre>
+//   </div>
+// </div>
--- src/components/GraphBuilder.js
+++ src/components/GraphBuilder.js
@@ -9,6 +9,20 @@ import './GraphBuilder.css';
 const graphBuilder = () => {
   const algorithms = ['Depth First Search', 'Breadth First Search', 'Dijkstra\'s Algorithm', 'A* Path Finding'];
 
+  const bfsCode = `
+  procedure BFS(G, root) is
+      let Q be a queue
+      label root as explored
+      Q.enqueue(root)
+      while Q is not empty do
+          v := Q.dequeue()
+          if v is the goal then
+              return v
+          for all edges from v to w in G.adjacentEdges(v) do
+              if w is not labeled as explored then
+                  label w as explored
+                  Q.enqueue(w)`;
+
   return (
     <div id="graphBuilder" className="graph-builder">
       <div id="controls" className="controls">
@@ -19,11 +33,13 @@ const graphBuilder = () => {
         />
         <Playback />
       </div>
-      <div id="graph" className="graph">
-        <Graph />
-      </div>
-      <div id="code" className="code">
-        <CodeViewer />
+      <div className="graph-code-container">
+        <div id="graph" className="graph">
+          <Graph />
+        </div>
+        <div id="code" className="code">
+          <CodeViewer code={bfsCode} />
+        </div>
       </div>
     </div>
   );
