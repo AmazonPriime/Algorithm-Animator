3a74e7ffbf5fca1c1a841aa5eb0a785ccb788593
Luke <2342915h@student.gla.ac.uk>
2021-11-21 19:59:34 +0000

    Added what the algorithms will look like

---
+++ src/algorithms/bfs.js
@@ -0,0 +1,16 @@
+export default {
+  name: 'Breadth First Search',
+  pseudocode: `procedure BFS(G, root) is
+    let Q be a queue
+    label root as explored
+    Q.enqueue(root)
+    while Q is not empty do
+        v := Q.dequeue()
+        if v is the goal then
+            return v
+        for all edges from v to w in G.adjacentEdges(v) do
+            if w is not labeled as explored then
+                label w as explored
+                Q.enqueue(w)`,
+  algorithm: () => {},
+};
