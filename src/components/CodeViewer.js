77d4a22e0b0ed03f66a87e078c29f4de6172df4d
Luke <2342915h@student.gla.ac.uk>
2022-01-25 01:51:34 +0000

    Updated the code viewer to have a tab system and setup groundwork for showing log messages

--- src/components/CodeViewer.js
+++ src/components/CodeViewer.js
@@ -10,22 +10,65 @@ const renderCode = (sections, codeSecNum) => sections.map((sec) => (
   </span>
 ));
 
+const renderLog = (log) => log.map((msg) => (
+  <span>
+    {`>> ${msg}`}
+  </span>
+));
+
+const changeTab = (e, tabName) => {
+  const evt = e;
+  // Get all elements with class='tabcontent' and hide them
+  const tabcontent = document.getElementsByClassName('tabcontent');
+  for (let i = 0; i < tabcontent.length; i += 1) {
+    tabcontent[i].style.display = 'none';
+  }
+
+  // Get all elements with class='tablinks' and remove the class 'active'
+  const tablinks = document.getElementsByClassName('tablinks');
+  for (let i = 0; i < tablinks.length; i += 1) {
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
+  }
+
+  // Show the current tab, and add an 'active' class to the button that opened the tab
+  document.getElementById(tabName).style.display = 'block';
+  evt.currentTarget.className += ' active';
+};
+
 const codeViewer = (props) => {
   const [expanded, setExpanded] = useState(false);
   const {
     code,
     codeSection,
+    log,
   } = props;
 
   const parsedCode = parseCodeSections(code);
+  const logMessages = log && log.length > 0 ? renderLog(log) : '';
 
   return (
-    <div className={expanded ? 'code-container expanded' : 'code-container'}>
-      <pre>
-        <code>
-          {renderCode(parsedCode, codeSection)}
-        </code>
-      </pre>
+    <div className={expanded ? 'code-container-expanded expanded' : 'code-container'}>
+      <div className="tab">
+        <button type="button" className="tablinks active" onClick={(e) => changeTab(e, 'pseudo')}>Pseudo</button>
+        <button type="button" className="tablinks" onClick={(e) => changeTab(e, 'log')}>Log</button>
+      </div>
+
+      <div id="pseudo" className="tabcontent">
+        <pre>
+          <code>
+            {renderCode(parsedCode, codeSection)}
+          </code>
+        </pre>
+      </div>
+
+      <div id="log" className="tabcontent">
+        <pre>
+          <code>
+            {logMessages}
+          </code>
+        </pre>
+      </div>
+
       <div className="toggle">
         <FontAwesomeIcon
           icon={faCaretDown}
